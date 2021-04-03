61179bceb21de391c767c249c5fd8f6376af2bd1
chngkiat <chng_kiat0402@hotmail.com>
2021-04-03 20:18:40 +0800

    Add Cumulative Sum solution and rewrote doc string for longest substring

--- all_algo.py
+++ all_algo.py
@@ -1,27 +1,30 @@
 class Solution:
-    def longestUniqueSubsttr(self, string):
-     
-        # last index of every character
-        last_idx = {}
+    def longestUniqueSubsttr(self, s):
+        '''
+        Finds length of Longest Unique Substring in String using start and end index. Solution is O(n) 
+        Inputs: s --> type: String : A String to search for longest substring
+        Output: Integer: The length of longest substring
+        '''
+        last_idx = {} # for hashing all the final index when scanning
         max_len = 0
-     
-        # starting index of current
-        # window to calculate max_len
         start_idx = 0
      
-        for i in range(0, len(string)):
-           
-            # Find the last index of str[i]
-            # Update start_idx (starting index of current window)
-            # as maximum of current value of start_idx and last
-            # index plus 1
-            if string[i] in last_idx:
-                start_idx = max(start_idx, last_idx[string[i]] + 1)
-     
-            # Update result if we get a larger window
-            max_len = max(max_len, i-start_idx + 1)
-     
-            # Update last index of current char.
-            last_idx[string[i]] = i
-     
+        for i in range(0, len(s)):
+            if s[i] in last_idx:
+                start_idx = max(start_idx, last_idx[s[i]] + 1) # Retrieve where the last noted starting index of the current letter is as its a repeat
+            max_len = max(max_len, i-start_idx + 1) # Find the length between the 2 repeated letters or adding one if its not a repeat
+            last_idx[s[i]] = i # hashing the position of the current letter
         return max_len
+
+    def maxCumSumInList(self,l):
+        '''
+        Uses Kadanes Algorithm to compute the maximum cumulative sum. 
+        Inputs: l --> type: list : A list of values for computing cumulative sum
+        Output: Float: The maximum cumsum in the list
+        '''
+        max_cumsum = 0
+        current_cumsum = 0
+        for values in l:
+            current_cumsum = max(0, current_cumsum + values)
+            max_cumsum = max(max_cumsum, current_cumsum)
+        return max_cumsum
